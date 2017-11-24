9913bcdb8bb7b632f4ffda8f9f2badd41ed4eff4
weikai <wkaichen@itri.org.tw>
2017-11-24 16:32:08 +0800

    Initial project.

---
+++ PUF/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace PUF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.btnCom = new System.Windows.Forms.Button();
+            this.cbbCOM = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labZero = new System.Windows.Forms.Label();
+            this.labOne = new System.Windows.Forms.Label();
+            this.btnPUF = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCom
+            // 
+            this.btnCom.Font = new System.Drawing.Font("PMingLiU", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCom.Location = new System.Drawing.Point(240, 2);
+            this.btnCom.Name = "btnCom";
+            this.btnCom.Size = new System.Drawing.Size(150, 30);
+            this.btnCom.TabIndex = 0;
+            this.btnCom.Text = "Open";
+            this.btnCom.UseVisualStyleBackColor = true;
+            this.btnCom.Click += new System.EventHandler(this.btnCom_Click);
+            // 
+            // cbbCOM
+            // 
+            this.cbbCOM.Font = new System.Drawing.Font("PMingLiU", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbbCOM.FormattingEnabled = true;
+            this.cbbCOM.Location = new System.Drawing.Point(110, 2);
+            this.cbbCOM.Name = "cbbCOM";
+            this.cbbCOM.Size = new System.Drawing.Size(120, 29);
+            this.cbbCOM.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 10;
+            this.dataGridView1.Size = new System.Drawing.Size(643, 643);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("PMingLiU", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(10, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "COM Port";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(43)))), ((int)(((byte)(134)))));
+            this.label3.Location = new System.Drawing.Point(662, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 50);
+            this.label3.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(253)))), ((int)(((byte)(8)))));
+            this.label4.Location = new System.Drawing.Point(662, 460);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 50);
+            this.label4.TabIndex = 12;
+            // 
+            // labZero
+            // 
+            this.labZero.Font = new System.Drawing.Font("PMingLiU", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labZero.Location = new System.Drawing.Point(712, 360);
+            this.labZero.Name = "labZero";
+            this.labZero.Size = new System.Drawing.Size(200, 54);
+            this.labZero.TabIndex = 13;
+            this.labZero.Text = "0:";
+            this.labZero.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labOne
+            // 
+            this.labOne.Font = new System.Drawing.Font("PMingLiU", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labOne.Location = new System.Drawing.Point(712, 460);
+            this.labOne.Name = "labOne";
+            this.labOne.Size = new System.Drawing.Size(200, 56);
+            this.labOne.TabIndex = 14;
+            this.labOne.Text = "1:";
+            this.labOne.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnPUF
+            // 
+            this.btnPUF.Enabled = false;
+            this.btnPUF.Font = new System.Drawing.Font("PMingLiU", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPUF.Location = new System.Drawing.Point(662, 60);
+            this.btnPUF.Name = "btnPUF";
+            this.btnPUF.Size = new System.Drawing.Size(200, 60);
+            this.btnPUF.TabIndex = 17;
+            this.btnPUF.Text = "PUF";
+            this.btnPUF.UseVisualStyleBackColor = true;
+            this.btnPUF.Click += new System.EventHandler(this.btnPUF_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(10, 750);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(850, 300);
+            this.chart1.TabIndex = 18;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea2";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(10, 1060);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(850, 300);
+            this.chart2.TabIndex = 19;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea3";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(10, 1370);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(850, 300);
+            this.chart3.TabIndex = 20;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea4";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Location = new System.Drawing.Point(10, 1680);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(850, 300);
+            this.chart4.TabIndex = 21;
+            this.chart4.Text = "chart4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btnPUF);
+            this.Controls.Add(this.labOne);
+            this.Controls.Add(this.labZero);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cbbCOM);
+            this.Controls.Add(this.btnCom);
+            this.Name = "Form1";
+            this.Text = "PUF Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCom;
+        private System.Windows.Forms.ComboBox cbbCOM;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labZero;
+        private System.Windows.Forms.Label labOne;
+        private System.Windows.Forms.Button btnPUF;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+    }
+}
+
---
+++ PUF/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace PUF
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort1 = new SerialPort();
+        private DataGridViewTextBoxColumn[] Col = new DataGridViewTextBoxColumn[64];
+        private int zeroCount = 0, oneCount = 0;
+        private delegate void Display();
+        private int[][] bitArray = new int[4][];
+
+        private Chart[] charts;
+        private Series[] bitSeries = new Series[4];
+        private int unit = 5;
+        private int chMax = 300;
+        private int[] xValues;
+        private int[][] yValues = new int[4][];
+
+        private Boolean receiving;
+        private Thread receiveThread;
+
+        private List<string> ExternalRead = new List<string>();
+        private Boolean ExternalReadCheck = false; Boolean pClick = false;
+        private int pCount = 0; int pComplete = 4105;
+        private int bitTh = 124;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < Col.Length; i++)
+            {
+                this.Col[i] = new DataGridViewTextBoxColumn();
+                this.Col[i].Width = 10;
+                this.Col[i].ReadOnly = true;
+                this.Col[i].Resizable = DataGridViewTriState.False;
+            }
+            dataGridView1.Columns.AddRange(Col);
+            dataGridView1.RowCount = 64;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Enabled = false;
+
+            for (int i = 0; i < bitArray.Length; i++)
+                bitArray[i] = new int[1024];
+
+            charts = new Chart[] { chart1, chart2, chart3, chart4 };
+            xValues = new int[chMax / unit];
+
+            for (int i = 0; i < xValues.Length; i++)
+                xValues[i] = unit * i;
+            for (int i = 0; i < yValues.Length; i++)
+                yValues[i] = new int[chMax / unit];
+            for (int i = 0; i < bitSeries.Length; i++)
+            {
+                bitSeries[i] = new Series();
+                charts[i].ChartAreas[0].AxisY.Maximum = 400;
+                Axis ax = charts[i].ChartAreas[0].AxisX;
+                ax.Interval = unit;
+                ax.IntervalOffset = 0;
+                ax.Minimum = 0;
+                ax.Maximum = chMax;
+                charts[i].Series.Add(bitSeries[i]);
+                bitSeries[i].ChartType = SeriesChartType.Column;
+                bitSeries[i].Name = "bitSeries" + i;
+                //bitSeries[i].IsValueShownAsLabel = true;
+
+                //set chart data
+                bitSeries[i].Points.Clear();
+                bitSeries[i].Points.DataBindXY(xValues, yValues[i]);
+                //set bitSeries axis x offset
+                foreach (DataPoint dp in bitSeries[i].Points) dp.XValue += ax.Interval / 2;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+
+            string[] serialPorts = SerialPort.GetPortNames();
+            foreach (string serialPort in serialPorts)
+            {
+                cbbCOM.Items.Add(serialPort);
+                if (cbbCOM.Items.Count > 0)
+                {
+                    cbbCOM.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void btnCom_Click(object sender, EventArgs e)
+        {
+            Button btnCom = (Button)sender;
+            if (btnCom.Text == "Open")
+            {
+                serialPort1.PortName = (string)cbbCOM.SelectedItem;
+                serialPort1.BaudRate = 115200;
+                serialPort1.DataBits = 8;
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Open();
+                serialPort1.DiscardInBuffer();
+                btnPUF.Enabled = true;
+                btnCom.Text = "Close";
+                receiving = true;
+                receiveThread = new Thread(DoReceive);
+                receiveThread.IsBackground = true;
+                receiveThread.Start();
+            }
+            else
+            {
+                receiving = false;
+                serialPort1.Close();
+                btnPUF.Enabled = false;
+                btnCom.Text = "Open";
+            }
+        }
+
+        private void btnPUF_Click(object sender, EventArgs e)
+        {
+
+            ExternalRead.Clear();
+            ExternalReadCheck = false;
+            for (int i = 0; i < bitArray.Length; i++)
+                Array.Clear(bitArray[i], 0, bitArray[i].Length);
+            for (int i = 0; i < yValues.Length; i++)
+                Array.Clear(yValues[i], 0, yValues[i].Length);
+            pCount = 0;
+            pClick = true;
+            zeroCount = 0;
+            oneCount = 0;
+            serialPort1.Write("z");
+            serialPort1.Write("o");
+            serialPort1.Write("r");
+        }
+        private void DoReceive()
+        {
+            string temp = "";
+            while (receiving)
+            {
+                if (serialPort1.BytesToRead > 0)
+                {
+                    temp = serialPort1.ReadLine();
+                    if (pClick)
+                    {
+                        this.Invoke((MethodInvoker)delegate () { btnPUF.Text = "Read...(" + pCount * 100 / pComplete + "%)"; });
+                        pCount++;
+                    }
+                    if (temp.Contains("External read") && ExternalReadCheck == false)
+                    {
+                        ExternalReadCheck = true;
+                        ExternalRead.Add(temp);
+                        //Console.Write(temp);
+                    }
+                    else if (ExternalReadCheck == true)
+                    {
+                        this.Invoke((MethodInvoker)delegate () { btnPUF.Text = "Read...(" + pCount * 100 / pComplete + "%)"; });
+                        pCount++;
+                        ExternalRead.Add(temp);
+                        //Console.Write(temp);
+
+                        if (temp.Contains("End"))
+                        {
+                            ExternalReadCheck = false;
+
+                            int[] numbers;
+                            for (int i = 1; i < 1025; i++)
+                            {
+                                //Console.Write(ExternalRead[i]);
+                                numbers = Regex.Matches(ExternalRead[i], "(-?[0-9]+)").OfType<Match>().Select(m => int.Parse(m.Value)).ToArray();
+                                bitArray[0][i - 1] = numbers[0];
+                                bitArray[1][i - 1] = numbers[1];
+                                bitArray[2][i - 1] = numbers[2];
+                                bitArray[3][i - 1] = numbers[3];
+                            }
+                            //Console.WriteLine(pCount);
+                            this.Invoke((MethodInvoker)delegate () { displayChart(); });
+                        }
+                    }
+                }
+            }
+        }
+        private void displayChart()
+        {
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                for (int j = 0; j < bitArray[i].Length; j++)
+                {
+                    yValues[i][bitArray[i][j] / unit]++;
+                }
+            }
+            for (int i = 0; i < bitSeries.Length; i++)
+            {
+                Axis ax = charts[i].ChartAreas[0].AxisX;
+                ax.Interval = unit;
+                ax.IntervalOffset = 0;
+                ax.Minimum = 0;
+                ax.Maximum = chMax;
+
+                //set chart data
+                bitSeries[i].Points.Clear();
+                bitSeries[i].Points.DataBindXY(xValues, yValues[i]);
+                //set bitSeries axis x offset
+                foreach (DataPoint dp in bitSeries[i].Points) dp.XValue += ax.Interval / 2;
+            }
+            displayMapArray();
+        }
+        private void displayMapArray()
+        {
+            int[] offsetX = new int[] { 0, 32, 0, 32 };
+            int[] offsetY = new int[] { 0, 0, 32, 32 };
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                for (int j = 0; j < bitArray[i].Length; j++)
+                {
+                    if (bitArray[i][j] <= bitTh)
+                    {
+                        dataGridView1.Rows[offsetY[i] + j / 32].Cells[offsetX[i] + j % 32].Style.BackColor = Color.FromArgb(52, 43, 134);
+                        zeroCount++;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[offsetY[i] + j / 32].Cells[offsetX[i] + j % 32].Style.BackColor = Color.FromArgb(249, 253, 8);
+                        oneCount++;
+                    }
+                }
+            }
+            double zeroPercent = Math.Round(zeroCount / 4096.0, 4);
+            double onePercent = 1 - zeroPercent;
+            labZero.Text = "0: " + zeroPercent * 100 + " %";
+            labOne.Text = "1: " + onePercent * 100 + " %";
+            btnPUF.Text = "PUF";
+        }
+    }
+}
